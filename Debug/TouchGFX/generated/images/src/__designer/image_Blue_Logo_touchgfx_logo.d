32eda80551efbd7043cd332e21fbc069d450314a
wegi <wegi01@outlook.com>
2026-02-15 12:02:39 +0100

    next commit

--- Debug/TouchGFX/generated/images/src/__designer/image_Blue_Logo_touchgfx_logo.d
+++
@@ -1,4 +0,0 @@
-TouchGFX/generated/images/src/__designer/image_Blue_Logo_touchgfx_logo.o: \
- ../TouchGFX/generated/images/src/__designer/image_Blue_Logo_touchgfx_logo.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
--- Debug/TouchGFX/generated/images/src/__generated/image_alternate_theme_images_logos_touchgfx.d
+++
@@ -1,4 +0,0 @@
-TouchGFX/generated/images/src/__generated/image_alternate_theme_images_logos_touchgfx.o: \
- ../TouchGFX/generated/images/src/__generated/image_alternate_theme_images_logos_touchgfx.cpp \
- ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
